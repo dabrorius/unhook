6b29469c14780262b314fe406775ce903b46add0
Filip Defar <filip.defar@toptal.com>
2018-11-09 17:17:58 +0100

    Retrieve current URL and block list

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,25 @@
 var app = new Vue({
   el: "#app",
   data: {
+    currentUrl: null,
     message: "Hello Vue!"
+  },
+  mounted() {
+    chrome.tabs.query({ currentWindow: true, active: true }, tabArray => {
+      const url = sanitizeURL(tabArray[0].url);
+      if (url !== "chrome:") {
+        this.currentUrl = url;
+      }
+    });
+
+    chrome.storage.sync.get({ blockList: [] }, result => {
+      const { blockList } = result;
+      this.message = blockList.join(",");
+      console.log(result);
+    });
   }
 });
+
+function sanitizeURL(url) {
+  return url.replace(/(https?:\/\/)?(www.)?([^\/]*).*/, "$3");
+}
